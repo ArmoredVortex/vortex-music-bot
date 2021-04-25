baf47c203147a7963d533e496009fcf76179abcf
ArmoredVortex <66690593+ArmoredVortex@users.noreply.github.com>
2021-04-25 13:51:47 +0530

    Added join and leave commands 🎉

--- commands/music/join.js
+++ commands/music/join.js
@@ -1,11 +1,15 @@
 module.exports = {
-    name: 'play',
-    execute(client,message,args) {
+    name: 'join',
+    execute(client,message) {
         if (!message.member.voice.channel) return message.channel.send(`You can only execute this command after joining a voice channel`);
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are Not in the same voice as the bot`);
 
-        if (!args[0]) return message.channel.send(`Want me to play a song for you? Atleast supply a title`);
-
-        client.player.play(message, args.join(" "), { firstResult: true });
+        try {
+            message.member.voice.channel.join();
+            message.channel.send(`Joined **${message.member.voice.channel.name}**`);
+        } catch (error) {
+            message.channel.send('There was an error joining the voice channel');
+            console.log(error);
+        }
     }
 }
--- commands/music/leave.js
+++ commands/music/leave.js
@@ -1,11 +1,13 @@
 module.exports = {
-    name: 'play',
-    execute(client,message,args) {
+    name: 'leave',
+    execute(client,message) {
         if (!message.member.voice.channel) return message.channel.send(`You can only execute this command after joining a voice channel`);
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`You are Not in the same voice as the bot`);
 
-        if (!args[0]) return message.channel.send(`Want me to play a song for you? Atleast supply a title`);
-
-        client.player.play(message, args.join(" "), { firstResult: true });
+        if(!message.guild.me.voice.channel) {
+            return message.channel.send('I am not in a voice channel');
+        }
+        message.guild.me.voice.channel.leave();
+        message.channel.send(`Left **${message.guild.me.voice.channel.name}**`);
     }
 }
