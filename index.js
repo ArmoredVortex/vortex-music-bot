04cdb2522a62cf5f5b8bb5a40121c7bdf45eccb7
ArmoredVortex <66690593+ArmoredVortex@users.noreply.github.com>
2021-05-17 17:50:00 +0530

    replaced http with express

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-// Heroku Deploy
-const http = require('http');
-http.createServer(onRequest).listen(process.env.PORT || 6000);
+// Importing express
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 require('dotenv').config();
 
