e96286d6e01379545be16ff848ba15c5dfb80340
ArmoredVortex <66690593+ArmoredVortex@users.noreply.github.com>
2021-05-22 21:33:34 +0530

    installed gtts and added tts command

---
+++ commands/utility/tts.js
@@ -0,0 +1,21 @@
+var gtts = require('node-gtts')('en');
+var path = require('path');
+module.exports = {
+	name: 'tts',
+	description: 'Converts Text provided to audio',
+	execute(client,message,args){
+		if(!args[0]){
+			message.channel.send('No Text provided');
+		}
+		let text = args.join(' ');
+		var filepath = path.join(__dirname, 'out.mp3');
+ 		message.channel.send('Processing =>'+'`'+text+'`');
+		gtts.save(filepath, text , () => {
+  			message.channel.send("Here's the result:", {
+  				files: [
+    				"commands/utility/out.mp3"
+  					]
+}			);
+		})
+	}
+}
